b5cb5bc50ae0ab4a218230caca75acfe40e998ca
SANTIAGO <tec.carlos.rocha@gmail.com>
2022-02-13 02:45:03 -0600

    ya se pudo y entendio mas :D

--- hola.py
+++ hola.py
@@ -2,4 +2,4 @@ entonces si yo hago cualquier cambio, con el comando git diff (diferencia)
 se podra ver los cambios hechos ed, pero tengo que estudiar python exactamente aun que
 hubo un pequeño detalle al momento de querer actualizar cambios en el git, no se actualizaban cambios D: 
 Los cambios fueron realizados correctamente, en git bash no era posible pero en git gui se realizo correctamentee
-bash otro :incidencia; esta linea no se guardo bien, pero listo creo ya se pudo arreglar
+bash otro :incidencia; esta linea no se guardo bien, pero listo creo ya se pudo arreglar, si ya se pudo :D

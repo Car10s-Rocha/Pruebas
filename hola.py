b871dcdfaab3c102b90ae2de719476f6ea6c6f74
SANTIAGO <tec.carlos.rocha@gmail.com>
2022-02-13 02:10:08 -0600

    se realizaron cambios para probar

---
+++ hola.py
@@ -0,0 +1,3 @@
+entonces si yo hago cualquier cambio, con el comando git diff (diferencia)
+se podra ver los cambios hechos ed, pero tengo que estudiar python exactamente aun que
+hubo un pequeño detalle al momento de querer actualizar cambios en el git, no se actualizaban cambios D:
